d915e3301591ef45627086ff1b6735242996af7b
GarlandNic <nicolas-garland@club-internet.fr>
2025-05-10 10:47:02 +0200

    création

---
+++ azurhyonVoyage/src/main/java/com/azurhyon/azurhyonVoyage/AzurhyonVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.azurhyon.azurhyonVoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurhyonVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurhyonVoyageApplication.class, args);
+	}
+
+}
---
+++ azurhyonVoyage/src/test/java/com/azurhyon/azurhyonVoyage/AzurhyonVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azurhyon.azurhyonVoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurhyonVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
